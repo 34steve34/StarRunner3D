f56987a2ee65ab3e02f4c34802688f8097b84cd2
34steve34 <stevesaad@gmail.com>
2026-03-13 08:50:48 -0400

    Add files via upload

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 // Universal version file - works for both ES6 modules and service workers
-const VERSION = "2.2.9";
+const VERSION = "2.3.0";
 
 // For service workers (must be first, before any exports)
 if (typeof self !== 'undefined' && typeof importScripts !== 'undefined') {
@@ -7,14 +7,4 @@ if (typeof self !== 'undefined' && typeof importScripts !== 'undefined') {
 }
 
 // ES6 export for modules (must be at top level)
-
 export { VERSION };
-
-
-
-
-
-
-
-
-
