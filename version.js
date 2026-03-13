50809ec6ec5ad56473c6790cf979e8a96cdeeb8b
34steve34 <stevesaad@gmail.com>
2026-03-13 06:15:13 -0400

    2.2.6

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 // Universal version file - works for both ES6 modules and service workers
-const VERSION = "2.2.5";
+const VERSION = "2.2.6";
 
 // For service workers (must be first, before any exports)
 if (typeof self !== 'undefined' && typeof importScripts !== 'undefined') {
@@ -9,6 +9,7 @@ if (typeof self !== 'undefined' && typeof importScripts !== 'undefined') {
 // ES6 export for modules (must be at top level)
 
 export { VERSION };
+
 
 
 
