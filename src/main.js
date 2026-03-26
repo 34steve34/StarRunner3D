c1546c496c57b503c3493dd90f1b01218898c453
34steve34 <stevesaad@gmail.com>
2026-03-25 22:47:12 -0400

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -2840,8 +2840,10 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('./service-worker.js')
             .then(registration => {
+                console.log('ServiceWorker registered:', registration.scope);
             })
             .catch(err => {
+                console.error('ServiceWorker registration failed:', err);
             });
     });
 }
